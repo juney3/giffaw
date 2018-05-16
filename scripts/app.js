eaa64e8337b864a37d813994c0957ff9d0562db9
juneyoshii <juney37@gmail.com>
2018-05-16 09:38:11 -0700

    implemented basic offset

--- scripts/app.js
+++ scripts/app.js
@@ -8,6 +8,8 @@ $(document).on("ready", function(){
 
   var apiKey = 'tFUeIOKZUTraApArgGf6U46jZO7NK3hK'
 
+var offset = 0;
+
 // Dreate the on submit form function
   $('form').on('submit', function(e) {
     e.preventDefault();
@@ -15,25 +17,50 @@ $(document).on("ready", function(){
     // Extract the input value
     gifCategory = $('input[type=text]').val();
 
-    // Make the API call
-    $.ajax({
-      method: 'GET',
-      url: giphyUrl,
-      data: {
-        q: gifCategory,
-        api_key: apiKey,
-        limit: 25,
-        rating: 'g'
-      },
-      success: gifSuccess,
-      error: gifError
-    });
-  })
 
+    // Make the API call for the first call
+    if (offset === 0) {
+      $.ajax({
+        method: 'GET',
+        url: giphyUrl,
+        data: {
+          q: gifCategory,
+          api_key: apiKey,
+          limit: 25,
+          rating: 'g'
+        },
+        success: gifSuccess,
+        error: gifError
+      });
+      offset +=25;
+      return offset;
+    }
+
+      else {
+        $.ajax({
+          method: 'GET',
+          url: giphyUrl,
+          data: {
+            q: gifCategory,
+            api_key: apiKey,
+            limit: 25,
+            rating: 'g',
+            offset: offset
+          },
+          success: gifSuccess,
+          error: gifError
+        });
+
+        offset +=25;
+        return offset;
+      }
+    })
+
+function consoleThis(response){
+  console.log(response);
+}
   // Define a function for a successful call
   function gifSuccess(response) {
-    console.log(response);
-
     // Create a loop to extract gif URLs for returned results, include the URL in HTML, and append the HTML to the gif-gallery div
     for (var i = 0; i < response.data.length; i++) {
       var imageUrl = response.data[i].images.fixed_height_small.url;
